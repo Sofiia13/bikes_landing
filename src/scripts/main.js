30af6f51f0041ebeae8a7152bd9d116052de0b33
Sofiia13 <sophiyastanish@gmail.com>
2025-10-01 20:10:55 +0300

    small changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector('.contacts__inputs');
 
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', function(event) {
   event.preventDefault();
   this.reset();
 });
